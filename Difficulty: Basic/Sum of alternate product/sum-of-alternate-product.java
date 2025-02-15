5efa993e4c0eb898f88e3a05470b00cd882fb6ff
Matta Chandra Naga Mouli <143476111+Mouli55555@users.noreply.github.com>
2025-02-15 07:52:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of alternate product/sum-of-alternate-product.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+class Solution {
+    public int altProduct(int[] arr) {
+        // Your code goes here
+        Arrays.sort(arr);
+        int sum = 0;
+        int j = arr.length-1;
+        for(int i=0;i<arr.length/2;i++){
+            sum += arr[i]*arr[j];
+            j--;
+        }
+        return sum;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.altProduct(arr);
+
+            System.out.println(res);
+
+            // System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
