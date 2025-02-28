f76d8598043ec85c35be872c5bfff4eb42f83bf7
Matta Chandra Naga Mouli <143476111+Mouli55555@users.noreply.github.com>
2025-03-01 01:17:43 +0530

    Time: 5 ms (99.71%) | Memory: 46.3 MB (20.52%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,23 +1,41 @@
 class Solution {
-    //OM 
-    public static boolean binarySearch(int[] arr, int tar){
-        int left = 0;
-        int right = arr.length-1;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(arr[mid]==tar) return true;
-            if(arr[mid]>tar){
-                right = mid-1;
-            }else{
-                left = mid+1;
-            }
-        }
-        return false;
-    }
+    ////OM 
+    // public static boolean binarySearch(int[] arr, int tar){
+    //     int left = 0;
+    //     int right = arr.length-1;
+    //     while(left<=right){
+    //         int mid = (left+right)/2;
+    //         if(arr[mid]==tar) return true;
+    //         if(arr[mid]>tar){
+    //             right = mid-1;
+    //         }else{
+    //             left = mid+1;
+    //         }
+    //     }
+    //     return false;
+    // }
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //     int n = matrix.length;
+    //     for(int i=0;i<n;i++){
+    //         if(binarySearch(matrix[i], target)) return true;
+    //     }
+    //     return false;
+    // }
+
+    //lets try another sol
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        for(int i=0;i<n;i++){
-            if(binarySearch(matrix[i], target)) return true;
+        int start = matrix[0].length-1;
+        int end = matrix.length-1;
+
+        int i = 0;
+
+        while(i<=end && start>=0){
+            if(matrix[i][start]==target) return true;
+            if(target<matrix[i][start]){
+                start--;
+            }else{
+                i++;
+            }
         }
         return false;
     }
