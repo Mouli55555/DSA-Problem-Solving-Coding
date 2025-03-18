a58487caa84ba8dae7c593f91d3ace2abc5584a0
Matta Chandra Naga Mouli <143476111+Mouli55555@users.noreply.github.com>
2025-03-18 21:38:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two arrays with Duplicate Elements/intersection-of-two-arrays-with-duplicate-elements.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.intersectionWithDuplicates(a, b);
+
+            // Sort the result
+            Collections.sort(res);
+
+            // Print the result
+            if (res.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int num : res) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public ArrayList<Integer> intersectionWithDuplicates(int[] a, int[] b) {
+        // code here
+        ArrayList<Integer> mouli = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int l = 0;
+        int r = 0;
+        int n = a.length, m = b.length;
+
+        while (l < n && r < m) {
+            if (a[l] == b[r]) {
+                if (mouli.isEmpty() || mouli.get(mouli.size() - 1) != a[l]) {
+                    mouli.add(a[l]);
+                }
+                l++;
+                r++;
+            } else if (a[l] < b[r]) {
+                l++;
+            } else {
+                r++;
+            }
+        }
+        return mouli;
+    }
+}
