732be1032ea682ab353a209142f617ea9749282a
Matta Chandra Naga Mouli <143476111+Mouli55555@users.noreply.github.com>
2025-03-19 22:41:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two Sorted Arrays with Distinct Elements/intersection-of-two-sorted-arrays-with-distinct-elements.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.intersection(a, b);
+            if (res.size() == 0) {
+                System.out.println("[]");
+                System.out.println("~");
+                continue;
+            }
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    ArrayList<Integer> intersection(int[] a, int[] b) {
+        // Your code here
+        ArrayList<Integer> mouli = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int l = 0;
+        int r = 0;
+        int n = a.length, m = b.length;
+
+        while (l < n && r < m) {
+            if (a[l] == b[r]) {
+                if (mouli.isEmpty() || mouli.get(mouli.size() - 1) != a[l]) {
+                    mouli.add(a[l]);
+                }
+                l++;
+                r++;
+            } else if (a[l] < b[r]) {
+                l++;
+            } else {
+                r++;
+            }
+        }
+        return mouli;
+    }
+}
