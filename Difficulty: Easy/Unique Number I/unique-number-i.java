bf9704016c6a62537f8fe8a11611a08c24b6e19e
Matta Chandra Naga Mouli <143476111+Mouli55555@users.noreply.github.com>
2025-06-01 23:25:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findUnique(int[] arr) {
+        // code here
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
