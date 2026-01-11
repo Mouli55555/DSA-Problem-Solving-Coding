117737480dbee7ea89be889a16e4df05ac9e70bd
Matta Chandra Naga Mouli <143476111+Mouli55555@users.noreply.github.com>
2026-01-11 07:25:51 +0530

    Time: 15 ms (91.72%) | Memory: 46.6 MB (28.34%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {  //:)
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        int[][] dp = new int[n1+1][n2+1];
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+(s1.charAt(i-1));
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        int s1Asci = 0;
+        for(int i=0;i<n1;i++){
+            s1Asci += (s1.charAt(i));
+        } 
+        int s2Asci = 0;
+        for(int i=0;i<n2;i++){
+            s2Asci += (s2.charAt(i));
+        }
+        return (s1Asci+s2Asci)-2*dp[n1][n2];
+    }
+}
